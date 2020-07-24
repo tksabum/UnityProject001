980c07ec3c9f37eec2a8b66e88dbd8f020638e5b
TC <37568161+momongss@users.noreply.github.com>
2020-07-24 15:11:53 +0900

    th

--- Platform_fighter_001/Assets/Scripts/Enemy_Move.cs
+++ Platform_fighter_001/Assets/Scripts/Enemy_Move.cs
@@ -5,22 +5,80 @@ using UnityEngine;
 public class Enemy_Move : MonoBehaviour
 {
     public int nextMove;
+    public int HP;
+    public Dictionary<string, int> bulletClass =
+        new Dictionary<string, int>();
+    public bool onHit;
+
     Rigidbody2D rigid;
-    Animator animator;
+    Animator anim;
     SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         Invoke("Think", 5);
+
+        HP = 10;
+        onHit = false;
+        bulletClass.Add("testBullet1", 1);
+        bulletClass.Add("testBullet5", 5);
+        bulletClass.Add("testBullet10", 10);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Contains("testBullet"))
+        {
+            Debug.Log("Hit!");
+            string bulletName = collision.gameObject.tag.ToString();
+            OnDamaged(collision.transform.position, bulletName);
+        }
+    }
+
+    void ReduceHP(int damage)
+    {
+
+    }
+
+    void OnDamaged(Vector2 targetPos, string bulletName)
+    {
+        // 상태 변경
+        onHit = true;
+
+        // 플레이어 튕겨남
+        float dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        float scalar = 2f;
+        rigid.AddForce(new Vector2(dirc * 1000f, 0), ForceMode2D.Impulse);
+
+        // HP
+        int damage;
+        bulletClass.TryGetValue(bulletName, out damage);
+        HP -= damage;
+
+        // Animation
+        spriteRenderer.color = new Color(1, 0.5f, 0.5f, 1);
+        anim.SetBool("isDamaged", true);
+        Invoke("OffDamaged", 0.2f);
+    }
+
+    void OffDamaged()
+    {
+        onHit = false;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+        anim.SetBool("isDamaged", false);
     }
 
     void FixedUpdate()
     {
-        // 이동
-        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+        // 타격을 받은 상태가 아니라면
+        if (!onHit)
+        {
+            // 이동
+            rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+        }
 
         // 지형확인
         Vector2 frontVec = new Vector2(rigid.position.x + 0.5f * nextMove, rigid.position.y);
@@ -31,12 +89,13 @@ public class Enemy_Move : MonoBehaviour
         if (rayHit.collider == null)
         {
             nextMove = -nextMove;
-            animator.SetInteger("WalkSpeed", nextMove);
+            anim.SetInteger("WalkSpeed", nextMove);
             if (nextMove != 0)
             {
                 spriteRenderer.flipX = nextMove > 0;
             }
         }
+        
     }
 
     void Think()
@@ -45,7 +104,7 @@ public class Enemy_Move : MonoBehaviour
         nextMove = Random.Range(-1, 2);
 
         // 애니메이션 처리
-        animator.SetInteger("WalkSpeed", nextMove);
+        anim.SetInteger("WalkSpeed", nextMove);
         if (nextMove != 0)
         {
             spriteRenderer.flipX = nextMove > 0;
