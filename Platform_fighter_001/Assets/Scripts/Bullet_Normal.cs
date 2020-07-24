b2aef25eade90a2ba41dda10720881d64a099519
tksabum <52379928+tksabum@users.noreply.github.com>
2020-07-24 16:28:12 +0900

    _

--- Platform_fighter_001/Assets/Scripts/Bullet_Normal.cs
+++ Platform_fighter_001/Assets/Scripts/Bullet_Normal.cs
@@ -21,6 +21,8 @@ public class Bullet_Normal : MonoBehaviour
         Debug.Log("collsion" + collision.gameObject.name);
         // 상속활용해서 체력을 가진 오브젝트라면 이벤트가 발생하도록 하면 좋을듯
 
+
+
         Destroy(gameObject); // 총알 삭제
     }
 }
